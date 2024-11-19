59ccc3751c22c31d8c2cc27dcf60a32521a04555
irineub <irineugmestre@gmail.com>
2024-11-19 03:37:38 -0400

    Estrutura Inicial

---
+++ src/app/pages/selected-clients/selected-clients.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-selected-clients',
+  standalone: true,
+  imports: [],
+  templateUrl: './selected-clients.component.html',
+  styleUrl: './selected-clients.component.scss'
+})
+export class SelectedClientsComponent {
+
+}
