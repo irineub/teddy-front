679b0f809773254d70c42d0ed78e332f87e48b2c
irineub <irineugmestre@gmail.com>
2024-11-19 03:50:29 -0400

    Environment e Service Para Consumir API

--- src/app/core/services/clients.service.ts
+++ src/app/core/services/clients.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { Client } from '../interfaces/client.model'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientsService {
+  private apiUrl = `${environment.apiUrl}/clients`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getClients(): Observable<Client[]> {
+    return this.http.get<Client[]>(this.apiUrl);
+  }
+
+  getClientById(id: number): Observable<Client> {
+    return this.http.get<Client>(`${this.apiUrl}/${id}`);
+  }
+
+  createClient(client: Client): Observable<Client> {
+    return this.http.post<Client>(this.apiUrl, client);
+  }
+
+  updateClient(id: number, client: Client): Observable<Client> {
+    return this.http.put<Client>(`${this.apiUrl}/${id}`, client);
+  }
+
+  deleteClient(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
